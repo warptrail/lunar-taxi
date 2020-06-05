67923280e7bd757dd713a921c68e1405f6867289
Ryan Whitmore <ethrealprojections@gmail.com>
2020-06-05 00:01:55 -0700

    Uploading the basic template files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const percentForm = document.querySelector('form');
+
+function isPercentOf(percentOf, number) {
+    let percent = parseFloat(((percentOf / number) * 100).toFixed(2));
+    return percent;
+}
+
+function displayPercentageOf(e) {
+    e.preventDefault();
+    let numberA = document.getElementById('input-number-a').value;
+    let numberB = document.getElementById('input-number-b').value;
+    let percentOfDemo = document.getElementById('output');
+    let percent = isPercentOf(numberA, numberB);
+    percentOfDemo.innerHTML += `
+        <p>${numberA} is ${percent}% of ${numberB}
+    `;
+    percentForm.reset();
+    document.getElementById('input-number-a').focus();
+}
+percentForm.onsubmit = displayPercentageOf;
+
+console.log(isPercentOf(740, 851));
